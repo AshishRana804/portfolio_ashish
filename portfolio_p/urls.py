4fee73011cea3c79c1b20f134bd6cb283f9d5463
AshishRana <ashishrana24804@gmail.com>
2025-11-13 03:25:10 +0530

    changes

--- portfolio_p/urls.py
+++ portfolio_p/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('secret-admin-9842/', admin.site.urls),
     path('', include('mywork.urls')),
 ]
